e1757cb298538ab1bb71c3f54ecd7b8cbf4d00d2
Fazliddin <fazliddindehkanoff@yandex.com>
2025-03-08 13:18:27 +0500

    feat: set up JWT authentication and Swagger

--- core/settings.py
+++ core/settings.py
@@ -20,9 +20,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'authentication',
     'stadium'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
